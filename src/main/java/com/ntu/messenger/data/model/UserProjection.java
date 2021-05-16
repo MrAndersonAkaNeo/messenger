c67b3b7e36409ac55c1a4457ed8290b5d7454c39
Kostiantyn Vozniuk <kv@xtendx.com>
2021-05-16 17:12:53 +0300

    Add extra fields to contact API

--- src/main/java/com/ntu/messenger/data/model/UserProjection.java
+++ src/main/java/com/ntu/messenger/data/model/UserProjection.java
@@ -2,6 +2,10 @@ package com.ntu.messenger.data.model;
 
 public interface UserProjection {
 
+    Long getId();
+
+    String getEmail();
+
     String getUsername();
 
     String getFirstname();
--- src/main/java/com/ntu/messenger/data/repository/UserRepository.java
+++ src/main/java/com/ntu/messenger/data/repository/UserRepository.java
@@ -17,8 +17,9 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Set<User> findAllByIdIn(List<Long> ids);
 
-    @Query(value = "SELECT u.username AS username, u.firstname AS firstname, u.lastname AS lastname FROM user u " +
-                   "WHERE u.id IN (SELECT c.contact_user_id c FROM contacts c WHERE c.user_id = :userId) " +
+    @Query(value = "SELECT u.id AS id, u.email AS email, u.username AS username, u.firstname AS firstname, u.lastname AS lastname " +
+                   "FROM user u " +
+                   "WHERE id IN (SELECT c.contact_user_id c FROM contacts c WHERE c.user_id = :userId) " +
                    "ORDER BY u.id " +
                    "LIMIT :limit OFFSET :offset",
             nativeQuery = true)
